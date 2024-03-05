d4a4f47ac3002c7456584d11df579f290f2ea18e
murad-mia <muradmia901@gmail.com>
2024-03-05 22:16:21 +0600

    Django

---
+++ Module 5/app.js
@@ -0,0 +1,50 @@
+var number = 1;
+var string = "murad Mia";
+// console.log(string);
+
+
+var num1 = 60.423;
+var num2 =40;
+var result = num1+num2;
+// console.log(parseInt(result));
+
+
+// var person={
+//     hands : 2,
+//     eye : 2,
+//     lag: 2,
+//     college:{
+//         name : "Narsing govt. collage",
+//         address : "Narsingdi",
+//     }
+// }
+// console.log(person.college.name);
+
+// array
+
+// var array = ['murad','Sadia','tanvir','parvej','simon'];
+// array.push('shiab')
+// array.pop()
+// array.unshift("simon")
+// array.shift("murad")
+// array.reverse()
+// console.log(array);
+// for(var i = 0;i < array.length;i++){
+//     if(array[i] == 'murad'){
+//         console.log("Murad is here");
+//     }
+// }
+//first problem
+// var num = 50;
+// if(num % 2 == 0){
+//     console.log("It is jor");
+// }else{
+//     console.log("it is not jor");
+// }
+
+// var array =[1,5,4,1,5,4,1,5,4,1,5,4,1,4,1,4,1,4,1,4];
+// console.log(array.sort(function(a,b)){
+//     return a-b;
+// });
+
+
